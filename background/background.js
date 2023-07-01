e71a36a51d801d5cea738dcce34bda418dd9c172
devlopersabbir <devlopersabbir@gmail.com>
2023-07-02 02:59:34 +0600

    finished

--- background/background.js
+++ background/background.js
@@ -1,5 +1,10 @@
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//   if (changeInfo.status === "complate" && tab.active) {
-//     console.log("URL changed:", changeInfo.url);
-//   }
-// });
+/**
+ * Name: Print Shortcut Google Chrome Extension
+ * Version: 0.0.1
+ * Manifest_version: 3
+ * Supported: Google Chrome | null
+ * Language: JavaScript && HTML && JSON
+ * Author: Sabbir Hossain Shuvo
+ * Author_Email: devlopersabbir@gmail.com
+ * Author_username: @devlopersabbir
+ */
--- content/content.js
+++ content/content.js
@@ -1,18 +1,44 @@
-const inti = () => {
-  const printButton = document.querySelector(
-    'button[onclick="printInvoice(913)"]'
-  );
-  if (printButton) {
-    printButton.addEventListener("click", () => {
-      console.log("clicked..");
+/**
+ * Name: Print Shortcut Google Chrome Extension
+ * Version: 0.0.1
+ * Manifest_version: 3
+ * Supported: Google Chrome | null
+ * Language: JavaScript && HTML && JSON
+ * Author: Sabbir Hossain Shuvo
+ * Author_Email: devlopersabbir@gmail.com
+ * Author_username: @devlopersabbir
+ */
+
+console.clear();
+const init = () => {
+  const checkPrintBtn = () => {
+    const printBtn = document.querySelector(
+      'button[onclick="printInvoice(923)"]'
+    );
+    if (printBtn) {
+      printBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        console.log("print btn clicked..");
+      });
+    } else {
+      console.log("btn not found");
+      setTimeout(checkPrintBtn, 1000); // Wait for 1 second and check again
+    }
+  };
+
+  const viewBtn = document.querySelector('a[onclick="orderView(923)"]');
+  if (viewBtn) {
+    viewBtn.addEventListener("click", () => {
+      console.log("clicked viewBtn.");
+      setTimeout(checkPrintBtn, 2000); // Wait for 2 seconds and check for printBtn
     });
   } else {
-    console.log("btn not fount!");
+    console.log("view btn not found!");
   }
 };
 
 if (document.readyState === "complete") {
-  inti();
+  init();
 } else {
-  inti();
+  init();
 }
